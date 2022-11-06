554d1df306d9af93f1f3b0f5de1daa5216e45252
DiamondStalker <cmoreno981@gmail.com>
2022-11-06 00:27:09 -0500

    a

--- control/vite.config.js
+++ control/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://control-de-asistencia.up.railway.app/:5000/",
+      "/api": "https://control-de-asistencia.up.railway.app:5000/",
     },
     host: true,
   },
