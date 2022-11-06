e5fad3b7a9d79ac268860ef0e5efc0f93fac2f3d
DiamondStalker <cmoreno981@gmail.com>
2022-11-06 11:08:39 -0500

    zx

--- control/vite.config.js
+++ control/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://worker-production-75a8.up.railway.app:5000/",
+      "/api": "https://worker-production-75a8.up.railway.app/",
     },
     host: true,
   },
