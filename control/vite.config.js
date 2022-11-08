e2bbedc45558cbd3f4949cf62c24597945e0a699
DiamondStalker <cmoreno981@gmail.com>
2022-11-07 23:15:19 -0500

    sad

--- control/vite.config.js
+++ control/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://worker-production-75a8.up.railway.app/",
+        target: "https://worker-production-75a8.up.railway.app:5000",
         changeOrigin: true,
         secure: false,
       },
