bcdfdc6ea179148a06516326c2699ea36c4046a4
DiamondStalker <cmoreno981@gmail.com>
2022-11-07 23:17:54 -0500

    sd

--- control/vite.config.js
+++ control/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
         target: "https://worker-production-75a8.up.railway.app:5000",
         changeOrigin: true,
         secure: false,
+        mode: 'no-cors'
       },
     },
     host: true,
