0a0c4e881429a0639ac35caf09c9a8427a9d928e
DiamondStalker <cmoreno981@gmail.com>
2022-11-07 23:22:46 -0500

    sdfs

--- control/vite.config.js
+++ control/vite.config.js
@@ -9,9 +9,11 @@ export default defineConfig({
         target: "https://worker-production-75a8.up.railway.app:5000",
         changeOrigin: true,
         secure: false,
-        mode: 'no-cors'
+        mode: 'no-cors',
+        rewrite: (path) => path.replace(/^\/api/, '')
       },
     },
+    cors:false,
     host: true,
   },
   plugins: [react()],
--- rutas/usuario.js
+++ rutas/usuario.js
@@ -53,6 +53,7 @@ router.get('/obtenerusuarios', (req, res) =>{
         }else{
             res.send(err)
         }
+        console.log(JSON.stringify(req.headers));
     })
 })
 
