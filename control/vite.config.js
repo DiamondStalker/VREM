05a44429e624af64fe3b85de841190829b94a784
DiamondStalker <cmoreno981@gmail.com>
2022-11-07 23:10:00 -0500

    sada

--- control/vite.config.js
+++ control/vite.config.js
@@ -5,7 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://worker-production-75a8.up.railway.app:5000",
+      "/api": {
+        target: "http://localhost:5000/",
+        changeOrigin: true,
+        secure: false,
+      },
     },
     host: true,
   },
